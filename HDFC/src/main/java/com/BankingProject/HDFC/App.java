96b1638a83ef93a12b4219b6c17ca328cd81c1ea
User <sanket.kshirsagar832@gmail.com>
2020-12-25 14:23:52 +0800

    Banking core Project

---
+++ HDFC/src/main/java/com/BankingProject/HDFC/App.java
@@ -0,0 +1,13 @@
+package com.BankingProject.HDFC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
